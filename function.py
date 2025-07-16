209b1b2d58b5a23f1b4e9c1a146192dd7c6de03b
achmadzaenni <achmadzaenniadriansyah1@gmail.com>
2025-07-16 11:58:04 +0700

    16/06/2025 -menambahkan alert untuk login -mengganti style button didalam page data

--- function.py
+++ function.py
@@ -12,45 +12,43 @@ from jiwer import wer
 from sqlalchemy.orm import Session as SQLSession
 from werkzeug.security import check_password_hash
 from database.db_config import Session, ProductTable, InvoiceBlur, Msuser
-
-def login_user(db_session: SQLSession, username: str, password: str):
-
-    user = db_session.query(Msuser).filter_by(usernm=username).first()
-
-    if not user:
-        return None
-
-    if user.pswd == password:
-        return user
-
-    return None
-
 def parse_row(row_text, full_text, filename):
     try:
         row_text = re.sub(r"[“![|~=j__—]", "", row_text)
         row_text = re.sub(r"\s{2,}", " ", row_text).strip()
-
         parts = row_text.split()
         if len(parts) < 5:
             return None
 
-        product_number = parts[0]
         line_total_str = parts[-1]
 
         if len(parts) >= 6 and "%" in parts[-2]:
             discount_str = parts[-2]
             unit_price_str = parts[-3]
             quantity_str = parts[-4]
-            description = " ".join(parts[1:-4]).strip().lstrip('/')
+            desc_parts = parts[1:-4]
         else:
             discount_str = "0%"
             unit_price_str = parts[-2]
             quantity_str = parts[-3]
-            description = " ".join(parts[1:-3]).strip().lstrip('/')
+            desc_parts = parts[1:-3]
 
-        quantity = int(re.sub(r"[^\d]", "", quantity_str))
-        unit_price = float(re.sub(r"[^\d.]", "", unit_price_str))
-        line_total = float(re.sub(r"[^\d.]", "", line_total_str))
+        def is_product_number(token):
+            return re.match(r"^(?=.*\d)[a-zA-Z0-9\-]{2,}$", token) is not None
+
+        if parts and is_product_number(parts[0]):
+            product_number = parts[0]
+            description = " ".join(desc_parts).strip().lstrip('/')
+        else:
+            product_number = ""
+            if len(parts) >= 6 and "%" in parts[-2]:
+                description = " ".join(parts[:-4]).strip().lstrip('/')
+            else:
+                description = " ".join(parts[:-3]).strip().lstrip('/')
+
+        quantity = int(re.sub(r"[^\d]", "", quantity_str)) if quantity_str else 0
+        unit_price = float(re.sub(r"[^\d.]", "", unit_price_str)) if unit_price_str else 0.0
+        line_total = float(re.sub(r"[^\d.]", "", line_total_str)) if line_total_str else 0.0
         discount = float(re.sub(r"[^\d.]", "", discount_str)) if discount_str else 0.0
 
         parsed_data = {
@@ -64,12 +62,71 @@ def parse_row(row_text, full_text, filename):
             'filename': filename
         }
 
-        # print(f"[PARSE OK] {parsed_data}")
         return parsed_data
 
     except Exception as e:
         print(f"Baris gagal di parsing: '{row_text}' | Error: {e}")
         return None
+
+# def extract_text_with_ocr(image):
+#     try:
+#         custom_config = r'--oem 3 --psm 6'
+#         page_text = pytesseract.image_to_string(image, config=custom_config, lang='eng')
+
+#         ocr_wer = wer(page_text, page_text) if page_text.strip() else 1.0 
+
+#         return page_text, ocr_wer
+
+#     except Exception as e:
+#         print(f"Error extracting text with OCR: {e}")
+#         return "", 1.0
+
+# def extract_image_with_ocr(image_path):
+#     try:
+#         image = cv2.imread(image_path)
+
+#         lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+#         l, a, b = cv2.split(lab)
+#         clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+#         cl = clahe.apply(l)
+#         merged = cv2.merge((cl, a, b))
+#         image = cv2.cvtColor(merged, cv2.COLOR_LAB2BGR)
+
+#         sharpen_kernel = np.array([[-1, -1, -1], [-1, 9.5, -1], [-1, -1, -1]])
+#         image = cv2.filter2D(image, -1, sharpen_kernel)
+
+#         image = cv2.fastNlMeansDenoisingColored(image, None, 5, 5, 7, 21)
+
+#         height, width = image.shape[:2]
+#         image = cv2.resize(image, (int(width * 2.5), int(height * 2.5)), interpolation=cv2.INTER_CUBIC)
+
+#         output_folder = "output"
+#         os.makedirs(output_folder, exist_ok=True)
+#         filename = os.path.basename(image_path)
+#         filename_no_ext, ext = os.path.splitext(filename)
+#         enhanced_path = os.path.join(output_folder, f"{filename_no_ext}_enhanced.webp")
+#         cv2.imwrite(enhanced_path, image)
+
+#         pil_img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)).convert("L")
+#         pil_img = pil_img.point(lambda p: 255 if p > 128 else 0)
+
+
+#         custom_config = r'--oem 3 --psm 6' 
+#         extracted_text = pytesseract.image_to_string(pil_img, config=custom_config, lang='eng')
+
+#         original_image = Image.open(image_path).convert("L")
+#         original_text = pytesseract.image_to_string(original_image, config=custom_config, lang='eng') 
+
+#         ocr_wer = wer(original_text, extracted_text) 
+
+#         print(f"Original = {original_text}") 
+#         print(f"Ekstrak = {extracted_text}") 
+#         print(f"Improvement WER (vs original image): {ocr_wer:.2%}") 
+#         return extracted_text, ocr_wer
+
+#     except Exception as e:
+#         print(f"Error extracting text with OCR: {e}")
+#         return None, None   
 
 def extract_text_with_ocr(image):
     try:
@@ -77,12 +134,13 @@ def extract_text_with_ocr(image):
         page_text = pytesseract.image_to_string(image, config=custom_config, lang='eng')
 
         ocr_wer = wer(page_text, page_text) if page_text.strip() else 1.0 
+        line_wer = wer_per_line(page_text, page_text)
 
-        return page_text, ocr_wer
+        return page_text, ocr_wer, line_wer
 
     except Exception as e:
         print(f"Error extracting text with OCR: {e}")
-        return "", 1.0
+        return None, None
 
 def extract_image_with_ocr(image_path):
     try:
@@ -98,10 +156,12 @@ def extract_image_with_ocr(image_path):
         sharpen_kernel = np.array([[-1, -1, -1], [-1, 9.5, -1], [-1, -1, -1]])
         image = cv2.filter2D(image, -1, sharpen_kernel)
 
-        image = cv2.fastNlMeansDenoisingColored(image, None, 5, 5, 7, 21)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
         height, width = image.shape[:2]
         image = cv2.resize(image, (int(width * 2.5), int(height * 2.5)), interpolation=cv2.INTER_CUBIC)
+        image = cv2.fastNlMeansDenoisingColored(image, None, 5, 5, 7, 21)
 
         output_folder = "output"
         os.makedirs(output_folder, exist_ok=True)
@@ -113,24 +173,26 @@ def extract_image_with_ocr(image_path):
         pil_img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)).convert("L")
         pil_img = pil_img.point(lambda p: 255 if p > 128 else 0)
 
-
-        custom_config = r'--oem 3 --psm 6' 
+        # OCR
+        custom_config = r'--oem 3 --psm 6'
         extracted_text = pytesseract.image_to_string(pil_img, config=custom_config, lang='eng')
 
+        # Original WER comparison
         original_image = Image.open(image_path).convert("L")
-        original_text = pytesseract.image_to_string(original_image, config=custom_config, lang='eng') 
+        original_text = pytesseract.image_to_string(original_image, config=custom_config, lang='eng')
 
-        ocr_wer = wer(original_text, extracted_text) 
+        ocr_wer = wer(original_text, extracted_text)
+        wer_line = wer_per_line(original_text, extracted_text)
 
-        print(f"Original = {original_text}") 
-        print(f"Ekstrak = {extracted_text}") 
-        print(f"Improvement WER (vs original image): {ocr_wer:.2%}") 
-        return extracted_text, ocr_wer
+        print(f"Original = {original_text}")
+        print(f"Ekstrak  = {extracted_text}")
+        print(f"Improvement WER (vs original image): {ocr_wer:.2%}")
+
+        return extracted_text, ocr_wer, wer_line
 
     except Exception as e:
         print(f"Error extracting text with OCR: {e}")
-        return None, None   
-
+        return None, None
 def wer_per_line(original_text, extracted_text):
     """
     Menghitung WER per baris antara original_text dan extracted_text.
@@ -161,13 +223,13 @@ def process_file(file_path, mode="product", text_override=None, useracid=None):
     elif file_path.endswith('.pdf'):
         images = convert_from_path(file_path, dpi=500)
         for img in images:
-            text, ocr_wer = extract_text_with_ocr(img)
+            text, ocr_wer, wer_per_line = extract_text_with_ocr(img)
             if text:
                 full_text += f"\n{text}"
         rows = full_text.split("\n") if full_text else []
 
     elif file_path.endswith('.webp'):
-        text, ocr_wer = extract_image_with_ocr(file_path)
+        text, ocr_wer, wer_per_line = extract_image_with_ocr(file_path)
         full_text = text
         rows = text.split("\n") if text else []
     else:
@@ -183,7 +245,6 @@ def process_file(file_path, mode="product", text_override=None, useracid=None):
         parsed = parse_row(row, full_text, filename)
         if parsed:
             all_rows.append(parsed)
-            # print(parsed)
         else:
             print(f"[PARSE FAIL] Gagal parsing baris: {row}")
 
