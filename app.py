d0c02594da33f11f26ea4ca4f0358a0c54522cbb
achmadzaenni <achmadzaenniadriansyah1@gmail.com>
2025-07-16 14:58:18 +0700

    16/07/2025 -mengubah app run nya disesuaikan dengan railway

--- app.py
+++ app.py
@@ -524,4 +524,6 @@ def invoice_count():
 if __name__ == "__main__":
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(host='0.0.0.0', debug=True, port=5000)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', debug=True, port=port)
